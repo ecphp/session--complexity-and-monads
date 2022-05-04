0be4065a355eb7d0f81f9cc3d407d08ed7b1a31d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-05-04 16:33:52 +0200

    Introduction: Update.

--- src/session/introduction/resources/complexity-example.php
+++ src/session/introduction/resources/complexity-example.php
@@ -2,22 +2,23 @@
 
 declare(strict_types=1);
 
-namespace App;
-
 final class Controller {
-
-    public function __invoke(int $id): string {
+    /**
+     * @Route /news/{id}
+     */
+    public function __invoke(string $id): ?string {
         $entity = $this->repository->find($id);
 
         if (null !== $entity) {
             if (1 === $entity->getAuthor()) {
                 if (null !== $entity->getTitle()) {
                     $title = $entity->getTitle();
-                    if (true === str_starts_with(strtolower($title), 'abc')) {
-                        return strtoupper($entity);
+                    if (str_starts_with(strtolower($title), 'abc')) {
+                        return strtoupper($title);
                     }
                 }
             }
         }
+        return null;
     }
 }
