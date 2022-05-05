d06381539626ce778e8569346c9b9133b18e39a2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-05-05 11:27:11 +0200

    History: Update.

--- src/session/history/resources/fp.js
+++ src/session/history/resources/fp.js
@@ -1,3 +1,5 @@
- const getOdds3 = arr => arr.filter(num => num % 2 !== 0);
+const input = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9];
 
- console.log(getOdds(arr)); // logs [ 1, 3, 5, 7, 9 ]
+const getOdds = arr => arr.filter(num => num % 2 !== 0);
+
+console.log(getOdds(input)); // logs [1, 3, 5, 7, 9]
--- src/session/history/resources/oop.js
+++ src/session/history/resources/oop.js
@@ -1,15 +1,10 @@
-const arr = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9]
+const input = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9];
+let odds = [];
 
-function getOdds(arr) {
-    let odds = [ ];
+for (let i = 0; i <= input.length; i++) {
+    if (i % 2 !== 0) {
+        odds.push(i);
+    }
+}
 
-    for(let i = 0; i < arr.length + 1; i++){
-        if ( i % 2 !== 0 ){
-            odds.push( i )
-        };
-    };
-
-    return odds
-};
-
-console.log(getOdds(arr)) // logs [1, 3, 5, 7, 9]
+console.log(odds); // logs [1, 3, 5, 7, 9]
