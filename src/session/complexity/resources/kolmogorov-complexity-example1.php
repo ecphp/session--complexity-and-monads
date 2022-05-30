86bd46ac75dcd89a9dc6fbf44e1030e304eca8b4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-05-30 16:44:40 +0200

    Complexity: Update.

--- src/session/complexity/resources/kolmogorov-complexity-example1.php
+++ src/session/complexity/resources/kolmogorov-complexity-example1.php
@@ -7,6 +7,10 @@ namespace App;
 final class KolmogorovComplexity
 {
     public function example1(): string {
-        return implode('',array_pad([],40,1));
+        return implode('',array_pad([],64,1));
+    }|\pause|
+
+    public function example2(): string {
+        return "317b773017df0ab62b15cd3f2ad17d7b13ab02f05f4943011ef8c4067d1ca0a5";
     }
 }
--- src/session/complexity/resources/kolmogorov-complexity-example2.php
+++ src/session/complexity/resources/kolmogorov-complexity-example2.php
@@ -7,6 +7,6 @@ namespace App;
 final class KolmogorovComplexity
 {
     public function example2(): string {
-        return "0a05a9e69e5757ac71c75a50418622624f042c97";
+        return "317b773017df0ab62b15cd3f2ad17d7b13ab02f05f4943011ef8c4067d1ca0a5";
     }
 }
